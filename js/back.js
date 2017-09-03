a1b3942c628087a196fe3b37a6f35f3507aabac0
floobydoo <matthewbrittain3d@gmail.com>
2017-09-03 16:11:37 -0700

    Fifth Commit
    Completed random backgrounds

--- js/back.js
+++ js/back.js
@@ -1,4 +1,4 @@
 var bgColorArray = ['images/drive.gif','images/fresh.gif','images/mondaymorning.jpg','images/sundaymorning.jpg','images/utopia.jpg'],
     selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
 
-$('body').css('background', 'url(' + selectBG + ')')
+document.getElementById("randback").src= selectBG;
