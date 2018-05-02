9b2d552441b9f4b24d1039b6ed98a21aa8a60f20
BuildTools <unconfigured@null.spigotmc.org>
2018-05-01 20:08:50 -0700

    Initiial Commit

--- js/back.js
+++
@@ -1,4 +0,0 @@
-var bgColorArray = ['images/drive.gif','images/fresh.gif','images/mondaymorning.jpg','images/sundaymorning.jpg','images/utopia.jpg'],
-    selectBG = bgColorArray[Math.floor(Math.random() * bgColorArray.length)];
-
-document.getElementById("randback").src= selectBG;
